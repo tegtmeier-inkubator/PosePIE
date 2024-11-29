dbd50f838616e85c72e3a40b96dab3ffa58d8970
Daniel Stolpmann <dstolpmann@tegtmeier-inkubator.de>
2024-11-29 10:08:03 +0100

    chore: add own OpenCV based main loop

--- game_script_base.py
+++ game_script_base.py
@@ -2,10 +2,16 @@ import os
 
 from abc import ABC, abstractmethod
 
+import cv2
 from ultralytics import YOLO
 
 from input_emulation.gamepad import Gamepad
 from pose.pose import Pose
+
+WINDOW_TITLE = "Camera"
+
+CAMERA_DEVICE = 3
+DISPLAY_CAMERA = True
 
 
 class GameScriptBase(ABC):
@@ -25,15 +31,34 @@ class GameScriptBase(ABC):
             model.export(format="engine", simplify=True, half=True, batch=1)
 
         model = YOLO(f"{MODEL}.engine")
-        results = model(source=3, show=True, save=False, conf=0.8, stream=True)
+        cap = cv2.VideoCapture(CAMERA_DEVICE)
 
-        for result in results:
-            self.pose.parse(result)
+        try:
+            while cap.isOpened():
+                success, frame = cap.read()
+                if not success:
+                    break
 
-            self.update()
+                results = model(frame, conf=0.8)
 
-            for gamepad in self.gamepad:
-                gamepad.update()
+                self.pose.parse(results[0])
+
+                self.update()
+
+                for gamepad in self.gamepad:
+                    gamepad.update()
+
+                if DISPLAY_CAMERA:
+                    annotated_frame = results[0].plot()
+                    cv2.imshow(WINDOW_TITLE, annotated_frame)
+
+                    if cv2.waitKey(1) & 0xFF == ord("q") or not cv2.getWindowProperty(WINDOW_TITLE, cv2.WND_PROP_VISIBLE):
+                        break
+        except KeyboardInterrupt:
+            pass
+
+        cap.release()
+        cv2.destroyAllWindows()
 
     @abstractmethod
     def setup(self) -> None:
