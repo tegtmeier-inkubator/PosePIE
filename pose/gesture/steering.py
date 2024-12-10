b014dbfdda94f57cd96193ebe0645be98cc7341a
Daniel Stolpmann <dstolpmann@tegtmeier-inkubator.de>
2024-12-10 14:30:24 +0100

    feat: detect steering based on distance between hands

--- pose/gesture/steering.py
+++ pose/gesture/steering.py
@@ -1,6 +1,8 @@
 from dataclasses import dataclass
 import math
 
+import numpy as np
+
 from pose.gesture.base import GestureBase
 from pose.keypoints import Keypoints
 from utils.vector import Vector2
@@ -14,13 +16,19 @@ class SteeringResult:
 
 class Steering(GestureBase[SteeringResult]):
     def __init__(self) -> None:
-        self.angle = 0.0
+        self._shoulder_width: float = 0.0
 
     def parse_keypoints(self, keypoints: Keypoints) -> SteeringResult:
+        if keypoints.left_shoulder.conf > 0.8 and keypoints.right_shoulder.conf > 0.8:
+            shoulder_width = abs(keypoints.left_shoulder.x - keypoints.right_shoulder.x)
+            alpha = 0.2 if self._shoulder_width > 0.0 else 1.0
+            self._shoulder_width = alpha * shoulder_width + (1 - alpha) * self._shoulder_width
+
         if keypoints.left_wrist.conf > 0.8 and keypoints.right_wrist.conf > 0.8:
-            detected = True
             steering_vector = Vector2(keypoints.left_wrist.xy - keypoints.right_wrist.xy)
-            angle = -math.degrees(math.atan2(steering_vector.y, steering_vector.x))
+
+            detected = bool(np.linalg.norm(steering_vector.xy) <= self._shoulder_width * 1.5)
+            angle = -math.degrees(math.atan2(steering_vector.y, steering_vector.x)) if detected else 0.0
         else:
             detected = False
             angle = 0.0
