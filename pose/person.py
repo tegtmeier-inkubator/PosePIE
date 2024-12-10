6006cc87bd23de56b0ec78d74709f8350546bb2d
Daniel Stolpmann <dstolpmann@tegtmeier-inkubator.de>
2024-12-10 14:28:49 +0100

    feat: cache gestures between updates

--- pose/person.py
+++ pose/person.py
@@ -1,3 +1,4 @@
+from typing import Any, Callable, TypeVar, cast
 import numpy as np
 
 import numpy.typing as npt
@@ -10,10 +11,14 @@ from pose.gesture.swiping import Swiping, SwipingResult
 from pose.keypoints import Keypoints
 from utils.side import Side
 
+T = TypeVar("T")
+
 
 class Person:
     def __init__(self) -> None:
         self.keypoints = Keypoints()
+
+        self.cache: dict[Callable[..., Any], Any] = {}
 
         self._left_arm_raising = ArmRaising(Side.LEFT)
         self._right_arm_raising = ArmRaising(Side.RIGHT)
@@ -28,32 +33,54 @@ class Person:
         keypoints: npt.NDArray[np.float64],
         keypoints_scores: npt.NDArray[np.float64],
     ) -> None:
+        self.cache = {}
+
         self.keypoints = Keypoints(keypoints, keypoints_scores)
 
+    @staticmethod
+    def _cache(func: Callable[..., T]) -> Callable[..., T]:
+        def wrapper(self: "Person") -> T:
+            if func in self.cache:
+                result = cast(T, self.cache[func])
+            else:
+                result = func(self)
+                self.cache[func] = result
+
+            return result
+
+        return wrapper
+
     @property
+    @_cache
     def left_arm_raising(self) -> ArmRaisingResult:
         return self._left_arm_raising.parse_keypoints(self.keypoints)
 
     @property
+    @_cache
     def right_arm_raising(self) -> ArmRaisingResult:
         return self._right_arm_raising.parse_keypoints(self.keypoints)
 
     @property
+    @_cache
     def jumping(self) -> JumpingResult:
         return self._jumping.parse_keypoints(self.keypoints)
 
     @property
+    @_cache
     def leaning(self) -> LeaningResult:
         return self._leaning.parse_keypoints(self.keypoints)
 
     @property
+    @_cache
     def steering(self) -> SteeringResult:
         return self._steering.parse_keypoints(self.keypoints)
 
     @property
+    @_cache
     def left_hand_swiping(self) -> SwipingResult:
         return self._left_hand_swiping.parse_keypoints(self.keypoints)
 
     @property
+    @_cache
     def right_hand_swiping(self) -> SwipingResult:
         return self._right_hand_swiping.parse_keypoints(self.keypoints)
