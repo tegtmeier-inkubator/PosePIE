58e8a9836bb2bb2da58fb4e8ea7f6757db64e880
Daniel Stolpmann <dstolpmann@tegtmeier-inkubator.de>
2025-01-10 16:37:07 +0100

    chore: fix array dimensions in tests

--- tests/test_pose_model.py
+++ tests/test_pose_model.py
@@ -22,19 +22,19 @@ class TestCorrectAspectRatio:
         bboxes, keypoints = correct_aspect_ratio(
             (1080, 1920),
             np.array([[0.0, 0.0, 1.0, 1.0]]),
-            np.array([[[0.0, 0.0, 0.5], [1.0, 1.0, 0.5], [0.5, 0.5, 0.5]]]),
+            np.array([[[0.0, 0.0], [1.0, 1.0], [0.5, 0.5]]]),
         )
         np.testing.assert_equal(bboxes, [[0.0, 0.21875, 1.0, 0.78125]])
-        np.testing.assert_equal(keypoints, [[[0.0, 0.21875, 0.5], [1.0, 0.78125, 0.5], [0.5, 0.5, 0.5]]])
+        np.testing.assert_equal(keypoints, [[[0.0, 0.21875], [1.0, 0.78125], [0.5, 0.5]]])
 
     def test_portrait(self) -> None:
         bboxes, keypoints = correct_aspect_ratio(
             (1920, 1080),
             np.array([[0.0, 0.0, 1.0, 1.0]]),
-            np.array([[[0.0, 0.0, 0.5], [1.0, 1.0, 0.5], [0.5, 0.5, 0.5]]]),
+            np.array([[[0.0, 0.0], [1.0, 1.0], [0.5, 0.5]]]),
         )
         np.testing.assert_equal(bboxes, [[0.21875, 0.0, 0.78125, 1.0]])
-        np.testing.assert_equal(keypoints, [[[0.21875, 0.0, 0.5], [0.78125, 1.0, 0.5], [0.5, 0.5, 0.5]]])
+        np.testing.assert_equal(keypoints, [[[0.21875, 0.0], [0.78125, 1.0], [0.5, 0.5]]])
 
 
 class TestFilterKeypointsAtEdge:
