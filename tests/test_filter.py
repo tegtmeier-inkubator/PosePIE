fix: set derivative to zero if time difference is zero but value did not change

tests/test_filter.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2024 Daniel Stolpmann <dstolpmann@tegtmeier-inkubator.de>
+# Copyright (c) 2024, 2025 Daniel Stolpmann <dstolpmann@tegtmeier-inkubator.de>
 #
 # This file is part of PosePIE.
 #
@@ -116,6 +116,7 @@ class TestDerivative:
         derivative = Derivative()
 
         np.testing.assert_approx_equal(derivative(np.array([1.0]), timestamp=0.0), 0.0)
+        assert derivative(np.array([1.0]), timestamp=0.0) == 0.0
         assert np.isinf(derivative(np.array([2.0]), timestamp=0.0))
 
     def test_without_timestamp(self) -> None:

utils/filter.py
@@ -56,7 +56,11 @@ class Derivative:
             diff = (
                 (value - self._old_value) / (timestamp - self._old_timestamp)
                 if not np.isclose(time_diff, 0.0)
-                else np.full_like(value, np.inf) if isinstance(value, np.ndarray) else np.inf
+                else (
+                    (np.full_like(value, np.inf) if isinstance(value, np.ndarray) else np.inf)
+                    if np.any(value - self._old_value)
+                    else (np.zeros_like(value) if isinstance(value, np.ndarray) else 0.0)
+                )
             )
         else:
             diff = np.zeros_like(value) if isinstance(value, np.ndarray) else 0.0